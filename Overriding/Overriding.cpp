006036572583743938ffb871c121fe6ba190a67d
Mihaly24 <fairuzk20@gmail.com>
2024-05-28 18:42:09 +0700

    Menambahkan class baseclass

--- Overriding/Overriding.cpp
+++ Overriding/Overriding.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class baseClass {
+public:
+    virtual void perkenalan() {
+        cout << "Hello saya function dair abse class";
+    }
+};
+
 int main()
 {
     
