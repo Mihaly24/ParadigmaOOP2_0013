ad80621f3fe6c96d1935af8d9af4c9462bf10f6c
Mihaly24 <fairuzk20@gmail.com>
2024-05-28 19:03:57 +0700

    Menambahkan class budi

--- MultiWaris/MultiWaris.cpp
+++ MultiWaris/MultiWaris.cpp
@@ -31,6 +31,17 @@ public:
     }
 };
 
+class budi : public pekerja, public pelajar {
+public:
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur),
+        orang(pUmur)
+    {
+        cout << "Budi dibuat" << endl;
+    }
+};
+
 int main()
 {
 
