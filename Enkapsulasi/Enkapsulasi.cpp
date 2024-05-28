5e79d55136da9fdfe68e919e2ffb07688409f825
Mihaly24 <fairuzk20@gmail.com>
2024-05-28 18:35:57 +0700

    Menyelesaikan int main

--- Enkapsulasi/Enkapsulasi.cpp
+++ Enkapsulasi/Enkapsulasi.cpp
@@ -21,4 +21,11 @@ int main()
     LampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
     LampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
     LampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    cout << LampuRumah.getSaklarNo(0) << endl;
+    cout << LampuRumah.getSaklarNo(1) << endl;
+    cout << LampuRumah.getSaklarNo(2) << endl;
+    cout << LampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
 }
