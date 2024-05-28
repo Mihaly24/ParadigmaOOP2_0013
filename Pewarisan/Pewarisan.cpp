030699957c1e36bcea3f266b6ace939aa3ced477
Mihaly24 <fairuzk20@gmail.com>
2024-05-28 18:48:53 +0700

    Menambahkan copas dari overriding dan menambahkan kata "final"

--- Pewarisan/Pewarisan.cpp
+++ Pewarisan/Pewarisan.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+using namespace std;
+
+class baseClass {
+public:
+    virtual void perkenalan() final {
+        cout << "Hello saya function dari base class";
+    }
+};
+
+class derivedClass : public baseClass {
+public:
+    //void perkenalan() {
+    //    cout << "Hello saya function dari derived class";
+   // }
+};
 
 int main()
 {
-    
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
 }
