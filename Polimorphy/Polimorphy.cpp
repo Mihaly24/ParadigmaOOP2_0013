fcc99d40fc53d60455669495caa13af00f784e8b
Mihaly24 <fairuzk20@gmail.com>
2024-05-28 17:43:38 +0700

    Menambahkan isi class seseorang

--- Polimorphy/Polimorphy.cpp
+++ Polimorphy/Polimorphy.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 class seseorang {
 public:
-
+    virtual void pesam() = 0;
+    //virtual void pesan() {
+    //      cout << "Pesan dari seseorang" << endl;
+    //}
 };
 
 int main()
