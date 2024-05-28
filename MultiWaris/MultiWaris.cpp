4274eb083d62479da2911fed54609a171c9afddd
Mihaly24 <fairuzk20@gmail.com>
2024-05-28 19:03:33 +0700

    Menambahkan class pekerja dan pelajar

--- MultiWaris/MultiWaris.cpp
+++ MultiWaris/MultiWaris.cpp
@@ -12,6 +12,25 @@ public:
     }
 };
 
+class pekerja :virtual public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+
+};
+
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+};
+
 int main()
 {
 
